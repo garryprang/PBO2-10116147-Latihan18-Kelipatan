654bc14a318ea68c6fe926cda7b4523f5768ab3b
Garry Prang <garryprang@grry>
2020-10-31 21:37:10 +0700

    Tugas - Latihan Pertemuan 2 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan18/kelipatan/PBO210116147Latihan18Kelipatan.java
@@ -0,0 +1,20 @@
+package pbo2.pkg10116147.latihan18.kelipatan;
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk menampilkan bilangan kelipatan 3,5.
+ */
+
+public class PBO210116147Latihan18Kelipatan {
+    public static void main(String[] args) {
+        for (double i= 3.5; i<=35; i+=3.5){
+            System.out.println(i);
+        }
+        System.out.println("");
+        System.out.println("Developed by Garry Prang");
+    }
+    
+}
